90e24adcce9ce1734ac6cd8739e838437a50eccd
xiaoyi <xiaoyi@shhxzq.com>
2017-06-15 16:49:48 +0800

    init

---
+++ reactcrm-generator/src/test/java/com/shhxzq/crm/react/generator/AppTest.java
@@ -0,0 +1,39 @@
+package com.shhxzq.crm.react.generator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author XiaoYi
+ * Created on 2017-06-02 17:04:30
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
