2a0532f8875ac5fdbd95b06f3964ecee4d5a0c8c
hussarch <hussarch@163.com>
2017-07-09 22:44:23 +0800

    add the conf data

---
+++ reactcrm-base/src/main/java/com/shhxzq/crm/react/base/page/model/templet/ConfMetaData.java
@@ -0,0 +1,17 @@
+package com.shhxzq.crm.react.base.page.model.templet;
+
+import java.util.Map;
+
+/**
+ * @ConfMetaData.java
+ * @author XiaoYi(hussarch@126.com)
+ * Created on 2017年7月9日, ©2017 some rights reserved
+ */
+public class ConfMetaData {
+    
+    private String mainPage;
+    private String renderedJs;
+    private Map<String, String> params;
+    private String clazz;
+    
+}
--- reactcrm-generator/src/main/java/com/shhxzq/crm/react/generator/Entity2MetaGenerator.java
+++ reactcrm-generator/src/main/java/com/shhxzq/crm/react/generator/Entity2MetaGenerator.java
@@ -1,13 +1,34 @@
 package com.shhxzq.crm.react.generator;
 
+import com.shhxzq.crm.react.base.page.model.templet.ConfMetaData;
+import com.shhxzq.crm.react.base.page.model.templet.ModaldialogMetaData;
+import com.shhxzq.crm.react.base.page.model.templet.TableMetaData;
+
 /**
  * @Entity2MetaGenerator.java
  * @author XiaoYi(hussarch@126.com)
  * Created on 2017年6月22日, ©2017 some rights reserved
  */
-    
-    
-    
-    
-    
+public class Entity2MetaGenerator {
+
+    public ConfMetaData getConfJson(Class clazz) {
+        return null;
+    }
+
+    public TableMetaData getTableJson() {
+        return null;
+    }
+
+    public ModaldialogMetaData getAddJson() {
+        return null;
+    }
+
+    public ModaldialogMetaData getUpdateJson() {
+        return null;
+    }
+
+    public ModaldialogMetaData getViewJson() {
+        return null;
+    }
+
 }
