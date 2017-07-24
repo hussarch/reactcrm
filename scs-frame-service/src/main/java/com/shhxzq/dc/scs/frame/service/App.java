1e420a2ee2e4483c55c30890f94166fe1075889d
xiaoyi <xiaoyi@shhxzq.com>
2017-07-24 18:16:32 +0800

    add the service

---
+++ scs-frame-service/src/main/java/com/shhxzq/dc/scs/frame/service/App.java
@@ -0,0 +1,13 @@
+package com.shhxzq.dc.scs.frame.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
